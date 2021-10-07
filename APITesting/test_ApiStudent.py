86b6086d67024e11aa35df9e0b75385ad5bd0df5
Angsuman Roy <angsuman1010@gmail.com>
2021-10-07 07:51:05 +0530

    saveAll

--- APITesting/test_ApiStudent.py
+++ APITesting/test_ApiStudent.py
@@ -2,7 +2,7 @@ import requests
 import json
 import jsonpath
 
-def test_addStudent_data():
+def test_post_Student_data():
     API_URL= "http://thetestingworldapi.com/api/studentsDetails"
     fileData = open('C:/Users/Angsuman/PycharmProjects/APITesting/RequestJson.json','r')
     json_request = json.loads(fileData.read())
@@ -16,3 +16,16 @@ def test_get_student_data():
     json_response = json.loads(response.text)
     id = jsonpath.jsonpath(json_response, 'data.id')
     assert id[0] == 461705
+
+def test_put_student_data():
+    API_URL= "http://thetestingworldapi.com/api/studentsDetails/461705"
+    fileData = open('C://Users//Angsuman//PycharmProjects//APITesting//RequestJsonPut.json','r')
+    jsonrequest = json.loads(fileData.read())
+    response = requests.put(API_URL,jsonrequest)
+    print(response.text)
+    
+def test_gettestforPut_student_data():
+    API_URM = "http://thetestingworldapi.com/api/studentsDetails/461705"
+    response = requests.get(API_URM)
+    json_response = response.json()
+    print(json_response)
